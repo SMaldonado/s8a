a71fc93175515c9412b3f2c0fd97fc43d6078f30
Sasha Maldonado <engineerajm@gmail.com>
2021-02-03 23:55:35 -0800

    building out butterfly testbench

--- fpga/test_fft.py
+++ fpga/test_fft.py
@@ -9,12 +9,25 @@ if __name__ == "__main__":
     sim = Simulator(m)
     sim.add_clock(1e-6) # important
 
-    def process():
-        yield
-        yield butterfly.start.eq(1)
-        yield
-        yield butterfly.start.eq(0)
+    def test_butterfly():
+        yield butterfly.tw_real.eq(1e20 - 1)
+        yield butterfly.tw_imag.eq(1e20 - 1)
+        for i in range(10):
+            yield butterfly.a_real.eq(i)
+            yield butterfly.b_real.eq(-i)
+            yield butterfly.a_imag.eq(-i)
+            yield butterfly.b_imag.eq(i)
+            yield
+            yield butterfly.start.eq(1)
+            yield
+            yield butterfly.start.eq(0)
 
-    sim.add_sync_process(process, domain="sync") # or sim.add_sync_process(process), see below
+            for j in range(3):
+                yield
+
+            got = yield(butterfly.a_prime_real)
+            print(got)
+
+    sim.add_sync_process(test_butterfly, domain="sync")
     with sim.write_vcd("sim/test.vcd", "sim/test.gtkw", traces=butterfly.ports()):
         sim.run_until(20e-6, run_passive=True)
