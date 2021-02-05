b7b715473885eb55c97085ad4f59e94011a3ce5f
Sasha Maldonado <engineerajm@gmail.com>
2021-02-04 23:38:48 -0800

    fixed some testbench math

--- fpga/test_fft.py
+++ fpga/test_fft.py
@@ -12,14 +12,17 @@ if __name__ == "__main__":
 
     def test_butterfly():
         tw_max = 2**20 - 1
-        const_tw = Const(tw_max)
-        yield butterfly.tw_real.eq(const_tw)
-        yield butterfly.tw_imag.eq(const_tw)
+        tw_real = tw_max
+        tw_imag = tw_max
+        const_tw_real = Const(tw_real)
+        const_tw_imag = Const(tw_imag)
+        yield butterfly.tw_real.eq(const_tw_real)
+        yield butterfly.tw_imag.eq(const_tw_imag)
 
         for i in range(10):
             ar = i
-            ai = i
-            br = -6
+            ai = -i
+            br = i
             bi = -i
             yield butterfly.a_real.eq(ar)
             yield butterfly.a_imag.eq(ai)
@@ -35,8 +38,10 @@ if __name__ == "__main__":
 
             gotr = yield(butterfly.a_prime_real)
             goti = yield(butterfly.a_prime_imag)
-            want = ((tw_max + 1j*tw_max) * (br + 1j*bi) / (2**20)) + (ar - 1j*ai)
-            print('{} = {} ({}), {}'.format(gotr, np.real(want), gotr==np.real(want), goti))
+            wantr = ar + ((tw_real*br) - (tw_imag*bi)) // (2**20)
+            wanti = ai + ((tw_real*bi) + (tw_imag*br)) // (2**20)
+
+            print('{} = {} ({}), {} = {} ({})'.format(gotr, wantr, gotr==wantr, goti, wanti, goti==wanti))
 
     sim.add_sync_process(test_butterfly, domain="sync")
     with sim.write_vcd("sim/test.vcd", "sim/test.gtkw", traces=butterfly.ports()):
