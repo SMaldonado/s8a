728906c58dcc22344fac6290dabb580bd0128b1a
Sasha Maldonado <engineerajm@gmail.com>
2021-02-05 18:51:09 -0800

    testbench now does error checking and actually sweeps an appropriately wide range of inputs. also it fails

--- fpga/test_fft.py
+++ fpga/test_fft.py
@@ -1,7 +1,9 @@
 from nmigen import *
 from nmigen.sim import Simulator, Delay, Settle
 from fft import Butterfly
+
 import numpy as np
+from random import randrange
 
 if __name__ == "__main__":
     m = Module()
@@ -19,11 +21,15 @@ if __name__ == "__main__":
         yield butterfly.tw_real.eq(const_tw_real)
         yield butterfly.tw_imag.eq(const_tw_imag)
 
-        for i in range(10):
-            ar = i
-            ai = -i
-            br = i
-            bi = -i
+        failed = 0
+        tests = 0
+        N = 21
+
+        for i in range(100):
+            ar = randrange(2**N) - (2**(N-1))
+            ai = randrange(2**N) - (2**(N-1))
+            br = randrange(2**N) - (2**(N-1))
+            bi = randrange(2**N) - (2**(N-1))
             yield butterfly.a_real.eq(ar)
             yield butterfly.a_imag.eq(ai)
             yield butterfly.b_real.eq(br)
@@ -38,10 +44,25 @@ if __name__ == "__main__":
 
             gotr = yield(butterfly.a_prime_real)
             goti = yield(butterfly.a_prime_imag)
-            wantr = ar + ((tw_real*br) - (tw_imag*bi)) // (2**20)
-            wanti = ai + ((tw_real*bi) + (tw_imag*br)) // (2**20)
+            wantr = ar + ((tw_real*br) - (tw_imag*bi)) // (2**(N-1))
+            wanti = ai + ((tw_real*bi) + (tw_imag*br)) // (2**(N-1))
 
-            print('{} = {} ({}), {} = {} ({})'.format(gotr, wantr, gotr==wantr, goti, wanti, goti==wanti))
+            if gotr != wantr or goti != wanti:
+                print('Test failed for a = {} + {}j, b = {} + {}j, tw = {} + {}j; {} = {} ({}), {} = {} ({})'.format(ar, ai, br, bi, tw_real, tw_imag, gotr, wantr, gotr==wantr, goti, wanti, goti==wanti))
+                failed += 1
+
+            gotr = yield(butterfly.b_prime_real)
+            goti = yield(butterfly.b_prime_imag)
+            wantr = ar - ((tw_real*br) - (tw_imag*bi)) // (2**(N-1))
+            wanti = ai - ((tw_real*bi) + (tw_imag*br)) // (2**(N-1))
+
+            if gotr != wantr or goti != wanti:
+                print('Test failed for a = {} + {}j, b = {} + {}j, tw = {} + {}j; {} = {} ({}), {} = {} ({})'.format(ar, ai, br, bi, tw_real, tw_imag, gotr, wantr, gotr==wantr, goti, wanti, goti==wanti))
+                failed += 1
+
+            tests += 2
+
+        print('butterfly failed {} of {} tests'.format(failed, tests))
 
     sim.add_sync_process(test_butterfly, domain="sync")
     with sim.write_vcd("sim/test.vcd", "sim/test.gtkw", traces=butterfly.ports()):
